c0412460cf1245c53535b9fad1b84fa4aff2fa79
nileshrajpure5888 <nileshrajpure037@gmail.com>
2026-05-09 19:29:49 +0530

    Update prc3.py

--- prc3.py
+++ prc3.py
@@ -1,10 +1,21 @@
 # Practical No. 03
 
-
 import pandas as pd
+
+# Read dataset
 df = pd.read_csv("Iris.csv")
-df.head()
-df.describe()
-df["SepalLengthCm"].describe()
-df.groupby("Species").describe()
-df.groupby("Species").describe().sum()
+
+# Display first 5 rows
+print(df.head())
+
+# Statistical summary of dataset
+print(df.describe())
+
+# Statistical summary of SepalLengthCm column
+print(df["SepalLengthCm"].describe())
+
+# Group data according to Species
+print(df.groupby("Species").describe())
+
+# Sum of grouped statistical values
+print(df.groupby("Species").describe().sum())
